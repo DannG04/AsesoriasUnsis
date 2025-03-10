6b62fc22a86b9e8122af7002d4a01551595c8096
DannG04 <2022060488_li@unsis.edu.mx>
2025-03-10 00:59:50 -0600

    Agregar archivos de asesorias_front

---
+++ asesorias_front/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Mi Aplicación</h1>
+    <ApiTest />
+  </div>
+</template>
+
+<script>
+import ApiTest from './components/ApiTest.vue';
+
+export default {
+  components: {
+    ApiTest
+  }
+}
+</script>
